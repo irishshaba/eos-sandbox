4a66e5ce73748f7b326c409fab338293ee9b3d52
Mike <mike@Kristinas-MacBook-Pro.local>
2018-08-29 01:09:25 -0400

    get everything working

--- 2/src/graphql-router.js
+++ 2/src/graphql-router.js
@@ -1,11 +1,9 @@
-const express = require('express')
 const Eos = require("eosjs")
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
-var path = require ('path');
 
-const app = express()
-const eos = Eos.Localnet();
+const express = require('express')
+const router = express.Router()
 
 var schema = buildSchema(`
     type BlockSummary {
@@ -18,13 +16,6 @@ var schema = buildSchema(`
     }
 `);
 
-class BlockSummary {
-    constructor(headId, transactionCount) {
-        this.blockId = headId
-        this.transactionCount = transactionCount
-    }
-}
-
 var root = {
     latest: async () => {
         info = await eos.getInfo({})
@@ -33,14 +24,10 @@ var root = {
     },
 };
 
-app.get('/graphql', graphqlHTTP({
+router.use('/', graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: false, // would it be cheating to just set this to true?
 }));
 
-app.get('/', async (req, res, next) => {
-    res.sendFile(path.join(__dirname + "/../dist/index.html"))
-})
-
-app.listen(8081, () => console.log('Example app listening on port 8081!'))
+module.exports = router;
